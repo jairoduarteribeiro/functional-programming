a0ab51070fc5403dfc18794eb271c9bde5b46de1
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-06-15 14:32:24 -0300

    Answer the second question

--- functions/challenge_1.js
+++ functions/challenge_1.js
@@ -10,3 +10,24 @@ function sum(a) {
 const sum3_4 = sum(3)(4)
 console.log(sum3_4(5))
 console.log(sum(3)(4)(5))
+
+// calc(x)(y)(fn) -> fn(x, y)
+function calc(x) {
+  return function (y) {
+    return function (fn) {
+      return fn(x, y)
+    }
+  }
+}
+
+function sub(a, b) {
+  return a - b
+}
+
+function mult(a, b) {
+  return a * b
+}
+
+const r1 = calc(10)(5)(sub)
+const r2 = calc(10)(5)(mult)
+console.log(r1, r2)
