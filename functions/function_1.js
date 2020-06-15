1f939f352bb2026564e9cdbf0e6667c863ce4a42
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-06-15 14:03:48 -0300

    Show how to pass parameters to a function

--- functions/function_1.js
+++ functions/function_1.js
@@ -11,3 +11,20 @@ const boaTarde = function () {
 }
 
 boaTarde()
+
+// Parameters
+function sum(a = 0, b = 0) {
+  return a + b
+}
+
+let result = sum(3, 4)
+console.log(result)
+
+result = sum(4, 5, 6, 7, 8)
+console.log(result)
+
+result = sum(3)
+console.log(result)
+
+result = sum()
+console.log(result)
