1ac864d09fc0713e33aba2a287fccfda4766d74b
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-06-18 10:57:17 -0300

    Show how to use map with objects

--- functions/callback_3.js
+++ functions/callback_3.js
@@ -6,3 +6,17 @@ const names = ['Ana', 'Bia', 'Gui', 'Lia', 'Rafa']
 const firstLetter = name => name[0]
 const letters = names.map(firstLetter)
 console.log(names, letters)
+
+const shoppingCart = [
+  { name: 'Caneta', amount: 10, price: 7.99 },
+  { name: 'Impressora', amount: 0, price: 649.50 },
+  { name: 'Caderno', amount: 4, price: 27.10 },
+  { name: 'Lápis', amount: 3, price: 5.82 },
+  { name: 'Tesoura', amount: 1, price: 19.20 }
+]
+
+const getName = product => product.name
+const getTotal = product => product.amount * product.price
+
+console.log(shoppingCart.map(getName))
+console.log(shoppingCart.map(getTotal))
