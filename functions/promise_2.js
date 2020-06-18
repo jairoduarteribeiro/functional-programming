b858e9fa9b52ffaea46fd61961f78f1af76bd7a1
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-06-18 16:47:18 -0300

    Show the same example using promises

--- functions/promise_2.js
+++ functions/promise_2.js
@@ -1,11 +1,24 @@
-setTimeout(function () {
-  console.log('Executando callback...')
+// setTimeout(function () {
+//   console.log('Executando callback...')
 
-  setTimeout(function () {
-    console.log('Executando callback...')
+//   setTimeout(function () {
+//     console.log('Executando callback...')
 
+//     setTimeout(function () {
+//       console.log('Executando callback...')
+//     }, 2000)
+//   }, 2000)
+// }, 2000)
+
+function wait(time = 2000) {
+  return new Promise(function (resolve) {
     setTimeout(function () {
-      console.log('Executando callback...')
-    }, 2000)
-  }, 2000)
-}, 2000)
+      console.log('Executando promise...')
+      resolve()
+    }, time)
+  })
+}
+
+wait()
+  .then(wait)
+  .then(wait)
