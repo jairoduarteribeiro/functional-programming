e2e40f468bc8d5bf9c8023e43f7c43c7e9a1b5f5
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-06-18 16:34:20 -0300

    Show how to use promises

---
+++ functions/promise_1.js
@@ -0,0 +1,10 @@
+const { resolve } = require("path")
+
+const firstElement = collection => collection[0]
+const lowerCase = str => str.toLowerCase()
+
+new Promise(resolve => resolve(['Ana', 'Bia', 'Carlos', 'Daniel']))
+  .then(firstElement) // get first string of array
+  .then(firstElement) // get first letter of string
+  .then(lowerCase)    // transform the letter to lower case
+  .then(console.log)  // print the result
