51c5f50e423cd7f54208544b9cfb369a5cde616d
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-06-18 11:15:24 -0300

    Show how to use filter

---
+++ functions/callback_4.js
@@ -0,0 +1,14 @@
+const shoppingCart = [
+  { name: 'Caneta', amount: 10, price: 7.99 },
+  { name: 'Impressora', amount: 0, price: 649.50 },
+  { name: 'Caderno', amount: 4, price: 27.10 },
+  { name: 'Lápis', amount: 3, price: 5.82 },
+  { name: 'Tesoura', amount: 1, price: 19.20 }
+]
+
+const amountGreaterThanZero = product => product.amount > 0
+
+const validProducts = shoppingCart
+  .filter(amountGreaterThanZero)
+
+console.log(validProducts)
