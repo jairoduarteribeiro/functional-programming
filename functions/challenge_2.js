402975e5a9f21c913fe039d995880c5b37fe84f0
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-06-18 15:08:08 -0300

    Answer 1st question

---
+++ functions/challenge_2.js
@@ -0,0 +1,12 @@
+const shoppingCart = [
+  { name: 'Caneta', amount: 10, price: 7.99, fragile: true },
+  { name: 'Impressora', amount: 1, price: 649.50, fragile: true },
+  { name: 'Caderno', amount: 4, price: 27.10, fragile: false },
+  { name: 'Lápis', amount: 3, price: 5.82, fragile: false },
+  { name: 'Tesoura', amount: 1, price: 19.20, fragile: true }
+]
+
+const average = shoppingCart
+  .filter(product => product.fragile)
+
+console.log(average)
