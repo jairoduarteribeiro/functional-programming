19e715b0859547895a010f7afcdc803cfa5d9ca4
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-06-18 22:51:34 -0300

    Wait using async/await

--- functions/async_await_1.js
+++ functions/async_await_1.js
@@ -4,9 +4,20 @@ function wait(time = 2000) {
   })
 }
 
-wait()
-  .then(() => console.log('Running promise 1...'))
-  .then(wait)
-  .then(() => console.log('Running promise 2...'))
-  .then(wait)
-  .then(() => console.log('Running promise 3...'))
+// wait()
+//   .then(() => console.log('Running promise 1...'))
+//   .then(wait)
+//   .then(() => console.log('Running promise 2...'))
+//   .then(wait)
+//   .then(() => console.log('Running promise 3...'))
+
+async function run() {
+  await wait(1500)
+  console.log('Async/Await 1...')
+  await wait(1500)
+  console.log('Async/Await 2...')
+  await wait(1500)
+  console.log('Async/Await 3...')
+}
+
+run()
