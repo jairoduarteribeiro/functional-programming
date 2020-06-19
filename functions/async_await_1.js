e4205046fa0f90967c8fd58dbca873dc92768344
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-06-18 23:05:53 -0300

    Wait for a value

--- functions/async_await_1.js
+++ functions/async_await_1.js
@@ -11,13 +11,24 @@ function wait(time = 2000) {
 //   .then(wait)
 //   .then(() => console.log('Running promise 3...'))
 
+function returnValue() {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(10), 5000)
+  })
+}
+
 async function run() {
+  const value = await returnValue()
+
   await wait(1500)
-  console.log('Async/Await 1...')
+  console.log(`Async/Await ${value}...`)
   await wait(1500)
-  console.log('Async/Await 2...')
+  console.log(`Async/Await ${value + 1}...`)
   await wait(1500)
-  console.log('Async/Await 3...')
+  console.log(`Async/Await ${value + 2}...`)
+
+  return value + 3
 }
 
 run()
+  .then(console.log)
