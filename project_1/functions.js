9029732d3c491b168ea4599e3fc63b39bb55c51b
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-06-30 22:25:49 -0300

    List files in a directory

--- project_1/functions.js
+++ project_1/functions.js
@@ -1,1 +1,11 @@
-module.exports = {}
+const fs = require('fs')
+const path = require('path')
+
+function getFilesFromDir(dirPath) {
+  const files = fs.readdirSync(dirPath)
+  return files.map(file => path.join(dirPath, file))
+}
+
+module.exports = {
+  getFilesFromDir
+}
--- project_1/index.js
+++ project_1/index.js
@@ -1,1 +1,6 @@
-const { } = require('./functions')
+const path = require('path')
+const fn = require('./functions')
+
+const subtitlesPath = path.join(__dirname, '..', 'data', 'subtitles')
+const files = fn.getFilesFromDir(subtitlesPath)
+console.log(files)
