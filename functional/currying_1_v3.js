2bc2721b05ec8861e96c7e65d106fbeb88f7b04e
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-07-05 10:48:17 -0300

    Apply currying on validation function

--- functional/currying_1_v3.js
+++ functional/currying_1_v3.js
@@ -11,18 +11,21 @@ function lengthBetween(min) {
   }
 }
 
-function validate(fn, value) {
-  try {
-    fn(value)
-  } catch (error) {
-    return { error }
+function validate(fn) {
+  return function (value) {
+    try {
+      fn(value)
+    } catch (error) {
+      return { error }
+    }
   }
 }
 
 const validLength = lengthBetween(4)(255)
 const validProductName = validLength('Invalid name')
+const validateProductName = validate(validProductName)
 
 const p1 = { name: 'A', price: 14.99, discount: 0.25 }
 const p2 = { name: 'AB', price: 14.99, discount: 0.25 }
-console.log(validate(validProductName, p1.name))
-console.log(validate(validProductName, p2.name))
+console.log(validateProductName(p1.name))
+console.log(validateProductName(p2.name))
