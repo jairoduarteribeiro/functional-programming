4b907cba4d8534cdcb7363a31f6b072453127c0b
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-07-05 09:46:23 -0300

    Sum numbers using reduce

--- functional/immutability_2.js
+++ functional/immutability_2.js
@@ -1,7 +1,10 @@
 const numbers = [4, 8, 3, 2, 9, 1, 9, 3]
 
-let total = 0
-for (let i = 0; i < numbers.length; i++) {
-  total += numbers[i]
-}
+// let total = 0
+// for (let i = 0; i < numbers.length; i++) {
+//   total += numbers[i]
+// }
+
+const sum = (a, b) => a + b
+const total = numbers.reduce(sum)
 console.log(total)
