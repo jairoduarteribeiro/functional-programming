c6569d6f5594613acd6f2cc0fa81aeeec4f16fd1
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-07-05 10:12:01 -0300

    Take care with deep copy

--- functional/immutability_3.js
+++ functional/immutability_3.js
@@ -1,13 +1,17 @@
 const person = {
   name: 'Maria',
   height: 1.76,
-  city: 'São Paulo'
+  city: 'São Paulo',
+  address: {
+    street: 'ABC'
+  }
 }
 
 function changeInformation(person) {
   const newPerson = { ...person }
   newPerson.name = 'João'
   newPerson.city = 'Fortaleza'
+  newPerson.address.street = '123'  // This line makes the function impure.
   return newPerson
 }
 
