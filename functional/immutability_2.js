3767301cbcbaf9d7bd4d60b21457ff6b209705b3
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-07-05 09:57:06 -0300

    Sum numbers using recursive function

--- functional/immutability_2.js
+++ functional/immutability_2.js
@@ -5,6 +5,12 @@ const numbers = [4, 8, 3, 2, 9, 1, 9, 3]
 //   total += numbers[i]
 // }
 
-const sum = (a, b) => a + b
-const total = numbers.reduce(sum)
+// const sum = (a, b) => a + b
+// const total = numbers.reduce(sum)
+
+function sum(array, total = 0) {
+  return array.length ? sum(array.slice(1), total + array[0]) : total
+}
+
+const total = sum(numbers)
 console.log(total)
