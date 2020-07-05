3ae03f76ed5d6c4c186fd4ff09ea7c37b8b30ffe
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-07-05 09:36:40 -0300

    Show how to make an array immutable

--- functional/immutability_1.js
+++ functional/immutability_1.js
@@ -2,7 +2,8 @@ function sort(array) {
   return [...array].sort((a, b) => a - b)
 }
 
-const numbers = [3, 1, 7, 9, 4, 6]
+const numbers = Object.freeze([3, 1, 7, 9, 4, 6])
+numbers[0] = 1000
 const orderedNumbers = sort(numbers)
 console.log(numbers, orderedNumbers)
 
